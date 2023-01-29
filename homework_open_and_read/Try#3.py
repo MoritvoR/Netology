31688cba4911a9c9322c971dd6051517ffdc93cf
Roman Nikitenko PD-73 <romanikitenko@mail.ru>
2023-01-29 19:46:49 +0700

    "Complete HW #3 open and read"

---
+++ homework_open_and_read/Try#3.py
@@ -0,0 +1,16 @@
+size = {}
+content = {}
+for i in range(1, 4):
+    with open('%s.txt' % i, 'r', encoding='utf-8') as file_txt:
+        read_file = file_txt.readlines()
+        #for i in read_file:
+            #i = i.strip()
+        size['%s.txt' % i] = len(read_file)
+        content['%s.txt' % i] = read_file
+with open('out_put.txt', 'w', encoding='utf-8') as out_put:
+    for now in sorted(size, key=size.get):
+        print(now, file=out_put)
+        print(size[now], file=out_put)
+        for i in content[now]:
+            print(i, file=out_put, end='')
+    
