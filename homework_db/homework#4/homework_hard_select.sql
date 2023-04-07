e886042f6392eb22e65a56791717c5ff717f06bb
Roman Nikitenko PD-73 <romanikitenko@mail.ru>
2023-04-07 23:04:01 +0700

    "Update homework_db#4"

--- homework_db/homework#4/homework_hard_select.sql
+++ homework_db/homework#4/homework_hard_select.sql
@@ -3,11 +3,10 @@ SELECT COUNT(performer_id), genre.title_genre
  INNER JOIN genre ON genre_performer.genre_id = genre.genre_id
  GROUP BY title_genre;
  
-SELECT COUNT(title_track), album.title_album
-  FROM album
- INNER JOIN track_list ON track_list.album_id = album.album_id
- WHERE year_of_release BETWEEN 2019 and 2020
- GROUP BY title_album;
+SELECT COUNT(track_list_id)
+  FROM track_list
+  JOIN album ON track_list.album_id = album.album_id
+ WHERE year_of_release BETWEEN 2019 and 2020;
  
 SELECT AVG(track_list.duration), album.title_album
   FROM album
@@ -30,12 +29,12 @@ SELECT DISTINCT collection.collection_title
   JOIN collection ON collection.collection_id = collection_track_list.collection_id
  WHERE performer_id = 10;
  
-SELECT album.title_album
-  FROM genre_performer
-  JOIN performer_album ON performer_album.performer_id = genre_performer.performer_id
-  JOIN album ON album.album_id = performer_album.album_id
- GROUP BY album.title_album
-HAVING COUNT(genre_performer.genre_id) > 1;
+SELECT DISTINCT(album.title_album)
+  FROM album
+  JOIN performer_album ON performer_album.album_id = album.album_id
+  JOIN genre_performer ON performer_album.performer_id = genre_performer.performer_id
+ GROUP BY album.album_id, genre_performer.performer_id
+HAVING COUNT(genre_id) > 1;
  
 SELECT title_track
   FROM track_list
@@ -45,24 +44,21 @@ SELECT title_track
  
 SELECT performer.name
   FROM performer
-  JOIN performer_album ON performer.performer_id = performer_album.performer_id
- WHERE album_id = (SELECT album_id
-                     FROM track_list
-                    WHERE duration = (SELECT MIN(duration) 
-									  FROM track_list)
-                    GROUP BY album_id);
+  JOIN performer_album ON performer_album.performer_id = performer.performer_id
+  JOIN album ON album.album_id = performer_album.album_id
+  JOIN track_list ON track_list.album_id = album.album_id
+ WHERE track_list.duration = (SELECT MIN(duration) 
+							    FROM track_list);
 					
 SELECT album.title_album
-FROM (SELECT album_id, COUNT(track_list_id) AS cnt
-        FROM track_list
-       GROUP BY album_id
-    ORDER BY COUNT(track_list_id)) AS my_table
-JOIN album ON my_table.album_id = album.album_id
-WHERE cnt = (SELECT COUNT(track_list_id)
-               FROM track_list
-              GROUP BY album_id
-              ORDER BY COUNT(track_list_id)
-              LIMIT 1);
+  FROM album
+  JOIN track_list ON track_list.album_id = album.album_id
+ GROUP BY album.album_id
+HAVING COUNT(track_list_id) = (SELECT COUNT(track_list_id)
+							     FROM track_list
+							    GROUP BY album_id
+							    ORDER BY 1
+							    LIMIT 1);
 					
 
 					
